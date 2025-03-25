513ba1bf48690aa0146c4873c89e5cd852969735
Sam Ensey <168656118+sensey0702@users.noreply.github.com>
2025-03-25 00:24:04 -0500

    start adding elements to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,22 @@
 import "./Header.css";
+import NewsExplorerLogo from "../../assets/NewsExplorerLogo.svg";
 
 function Header() {
-  return <header className="header">HEADER</header>;
+  return (
+    <header className="header">
+      <img
+        src={NewsExplorerLogo}
+        alt="News Explorer Logo"
+        className="header__logo"
+      />
+      <div className="header__button-wrapper">
+        <button className="header__button header__button--home">Home</button>
+        <button className="header__button header__button--sign-in">
+          Sign In
+        </button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
