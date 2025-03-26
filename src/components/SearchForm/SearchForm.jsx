fc5343d4007faaaff59c26a85060492cc963641f
Sam Ensey <168656118+sensey0702@users.noreply.github.com>
2025-03-25 23:51:18 -0500

    update styling and semantics for searchform component, button not done

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,7 +1,28 @@
 import "./SearchForm.css";
 
 function SearchForm() {
-  return <section className="search-form">SEARCHFORM</section>;
+  return (
+    <section className="search">
+      <h1 className="search__title">What's going on in the world?</h1>
+      <p className="search__description">
+        Find the latest news on any topic and save them in your personal
+        account.
+      </p>
+      <form className="search__form">
+        <label htmlFor="search" className="search__label">
+          <input
+            id="search"
+            type="text"
+            className="search__input"
+            placeholder="Enter topic"
+          />
+          <button type="submit" className="search__button">
+            SEARCH
+          </button>
+        </label>
+      </form>
+    </section>
+  );
 }
 
 export default SearchForm;
