479f7296deb053c37c2ce4c34305e5afc165da46
Sam Ensey <168656118+sensey0702@users.noreply.github.com>
2025-03-26 23:49:46 -0500

    add styling for about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,22 @@
 import "./About.css";
 
+import author from "../../assets/author.png";
+
 function About() {
-  return <section className="about">ABOUT</section>;
+  return (
+    <section className="about">
+      <img src={author} alt="photo of author" className="about__image" />
+      <div className="about__wrapper">
+        {" "}
+        <h1 className="about__title">About the author</h1>
+        <p className="about__paragraph">
+          Hello! My name is Samantha Ensey and I am a Software Engineering
+          student with TripleTen. I am currently a chaos manager at home for my
+          three year old and one year old. Welcome to my NewsExplorer app!
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default About;
