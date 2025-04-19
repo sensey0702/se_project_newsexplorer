8603b2961c1cff88d99ddacdf6b536a7a41582ee
Sam Ensey <168656118+sensey0702@users.noreply.github.com>
2025-04-19 10:24:20 -0500

    updated padding in savednews

--- src/components/SavedNews/SavedNews.jsx
+++ src/components/SavedNews/SavedNews.jsx
@@ -1,5 +1,7 @@
 import "./SavedNews.css";
 
+import NewsCardList from "../NewsCardList/NewsCardList";
+
 import { keywords } from "../../utils/constants";
 import { articles } from "../../utils/constants";
 
@@ -29,6 +31,7 @@ function SavedNews() {
           {formatKeywords(keywords)}
         </span>
       </p>
+      <NewsCardList />
     </section>
   );
 }
