28eea1468e5e5da1938c7da1abbcf10ab3143285
Sam Ensey <168656118+sensey0702@users.noreply.github.com>
2025-04-23 22:07:30 -0500

    add tablet view for about component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,11 +9,14 @@ function About() {
       <div className="about__wrapper">
         {" "}
         <h1 className="about__title">About the author</h1>
-        <p className="about__paragraph">
-          Hello! My name is Samantha Ensey and I am a Software Engineering
-          student with TripleTen. I am currently a chaos manager at home for my
-          three year old and one year old. Welcome to my NewsExplorer app!
-        </p>
+        <div className="about__paragraph-wrapper">
+          <p className="about__paragraph">
+            Hello! My name is Samantha Ensey and I am a Software Engineering
+            student with TripleTen. I am currently a chaos manager at home for
+            my three year old and one year old.
+          </p>
+          <p className="about__paragraph">Welcome to my NewsExplorer app!</p>
+        </div>
       </div>
     </section>
   );
