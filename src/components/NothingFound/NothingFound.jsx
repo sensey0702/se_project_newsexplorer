30e06bc2f3bac9333377702fb40fabae1fbcba56
Sam Ensey <168656118+sensey0702@users.noreply.github.com>
2025-05-13 07:12:25 -0500

    finish styling nothingfound component

--- src/components/NothingFound/NothingFound.jsx
+++ src/components/NothingFound/NothingFound.jsx
@@ -5,7 +5,11 @@ import notFound from "../../assets/not-found-img.png";
 function NothingFound() {
   return (
     <div className="nothing-found">
-      <img src={notFound} alt="" className="nothing-found__img" />
+      <img
+        src={notFound}
+        alt="Not found sad emoji"
+        className="nothing-found__img"
+      />
       <h2 className="nothing-found__title">Nothing found</h2>
       <p className="nothing-found__message">
         Sorry, but nothing matched your search terms.
