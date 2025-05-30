b6bcd0b7d24a154a171da5ca2bfa5d34c3a8b509
Sam Ensey <168656118+sensey0702@users.noreply.github.com>
2025-05-30 00:24:40 -0500

    add console log statments for env url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,13 @@
+console.log("NODE_ENV:", import.meta.env.NODE_ENV);
+console.log("VITE_NEWS_API_URL_PROD:", import.meta.env.VITE_NEWS_API_URL_PROD);
+console.log("VITE_NEWS_API_URL_DEV:", import.meta.env.VITE_NEWS_API_URL_DEV);
+
 export const newsApiBaseUrl =
   import.meta.env.NODE_ENV === "production"
     ? import.meta.env.VITE_NEWS_API_URL_PROD
     : import.meta.env.VITE_NEWS_API_URL_DEV;
+
+console.log("Final API URL being used:", newsApiBaseUrl);
 
 export const MOCK_USER = {
   email: "test@test.com",
